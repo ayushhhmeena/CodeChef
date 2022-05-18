a95b3c4892c580211ea4d6f2a7cf29f3368e3d0e
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2022-05-18 23:00:41 +0530

    Add files via upload

---
+++ My_very_1_st_contest.cpp
@@ -0,0 +1,14 @@
+/**
+ *    author:  isilent
+ *    Q : MY1STCONTEST
+ **/
+#include <bits/stdc++.h>
+int main(){
+    using std::cin;
+    using std::cout;
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N,A,B;
+    cin>>N>>A>>B;
+    cout<<N-A<<" "<<(N-A)-B;
+}
