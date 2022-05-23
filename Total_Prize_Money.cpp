1050203009c8eaa40df7799df85363c13e663c25
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2022-05-23 17:39:29 +0530

    Add files via upload

---
+++ Total_Prize_Money.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:  isilent     
+ *    Q : PRIZEPOOL 
+**/
+#include<bits/stdc++.h>
+int main(){
+    using std::cout;
+    using std::cin;
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        int X,Y;
+        cin>>X>>Y;
+        cout<<(10*X)+(90*Y)<<'\n';
+    }
+}
