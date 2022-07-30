6859ac6797110cb26096236720b8a3a30542fd85
AYUSH MEENA <87579312+isiiilent@users.noreply.github.com>
2022-07-30 20:52:30 +0530

    Add files via upload

---
+++ OFFICE.cpp
@@ -0,0 +1,19 @@
+/**
+ *    author:  isilent     
+ *    Q : OFFICE
+**/
+#include<bits/stdc++.h>
+int main(){
+    using std::cout;
+    using std::cin;
+    std::ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        int X,Y;
+        cin>>X>>Y;
+        cout<<(4*X)+(Y)<<'\n';
+    }
+    return 0;
+}
