d2467b43ded9cf8500a026caa0639bd7ba8c24d1
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-11-29 23:27:06 +0530

    Create Water Park.cpp

---
+++ Water Park.cpp
@@ -0,0 +1,16 @@
+/* need_job
+   CodeChef
+   Ayush M
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+int main(){
+    int w,h; cin>>w>>h;
+    if(h<=130 && w>=60){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
