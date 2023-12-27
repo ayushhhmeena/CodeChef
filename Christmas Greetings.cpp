a566050405b497282448ec96706dc424152b4ab7
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2023-12-27 21:44:55 +0530

    Create Christmas Greetings.cpp

---
+++ Christmas Greetings.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void soln(){
+    ll n; cin>>n;
+    if(n==25){
+        cout<<"CHRISTMAS";
+    }else{
+        cout<<"ORDINARY";
+    }
+}
+int main(){
+    ll t=1; 
+    //cin>>t;
+    while(t--){
+        soln();
+    }
+    return 0;
+}
