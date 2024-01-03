24ced4f550a745b31a12e529bd32e35c3519095c
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2024-01-03 20:58:06 +0530

    Create Codechef Round.cpp

---
+++ Codechef Round.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void soln(){
+    ll n; cin>>n;
+    if(n==4){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
+int main(){
+    ll t=1; 
+    //cin>>t;
+    while(t--){
+        soln();
+    }
+    return 0;
+}
