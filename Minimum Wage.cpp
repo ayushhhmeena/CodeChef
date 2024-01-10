2cdf337f85dbbf15191b9de630f7df28db17f7fe
AYUSH MEENA <87579312+ayushhhmeena@users.noreply.github.com>
2024-01-10 22:28:06 +0530

    Create Minimum Wage.cpp

---
+++ Minimum Wage.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void soln(){
+    ll n; cin>>n;
+    if(n>11){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+}
+int main(){
+    ll t=1; 
+    //cin>>t;
+    while(t--){
+        soln();
+    }
+    return 0;
+}
